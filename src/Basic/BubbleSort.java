7033200d0fda10116f09d9ffc46e0c83859eb578
z4n9 <arthur1820@z4n9pc.com>
2025-03-05 15:03:03 +0500

    Bubble sort task(upd)

--- src/Basic/BubbleSort.java
+++ src/Basic/BubbleSort.java
@@ -1,4 +1,31 @@
 package Basic;
 
+import java.util.ArrayList;
+
 public class BubbleSort {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            arr.add((int) (Math.random() * 1000 ));
+        }
+        for (Integer i : arr) {
+            System.out.println(i);
+        }
+        getSorted(arr);
+        System.out.println("Array is sorted");
+        for (Integer i : arr) {
+            System.out.println(i);
+        }
+    }
+    public static void getSorted(ArrayList<Integer> arr){
+        for (int i = 0; i < arr.size()-1; i++) {
+            for (int j = 0; j < arr.size()-1-i; j++) {
+                if(arr.get(j) > arr.get(j+1)){
+                    int temp = arr.get(j);
+                    arr.set(j, arr.get(j+1));
+                    arr.set(j+1, temp);
+                }
+            }
+        }
+    }
 }
--- src/Basic/MaxMinAvg.java
+++ src/Basic/MaxMinAvg.java
@@ -1,4 +1,44 @@
 package Basic;
 
+import java.util.ArrayList;
+
 public class MaxMinAvg {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            arr.add((int) (Math.random() * 10));
+        }
+        for (Integer i : arr) {
+            System.out.println(i);
+        }
+        System.out.println();
+        System.out.println(maxValue(arr));
+        System.out.println(minValue(arr));
+        System.out.println(avgValue(arr));
+    }
+    public static int maxValue(ArrayList<Integer> arr){
+        int max = 0;
+        for (int i : arr) {
+            if (i > max){
+                max = i;
+            }
+        }
+        return max;
+    }
+    public static int minValue(ArrayList<Integer> arr){
+        int min = maxValue(arr);
+        for (int i : arr) {
+            if (i < min){
+                min = i;
+            }
+        }
+        return min;
+    }
+    public static int avgValue(ArrayList<Integer> arr){
+        int avg = 0;
+        for (int i : arr) {
+            avg += i;
+        }
+        return avg / arr.size();
+    }
 }
