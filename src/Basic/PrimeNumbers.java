3e12006f763a9a527878227662bdf4fd1536727f
z4n9 <arthur1820@z4n9pc.com>
2025-03-07 23:18:09 +0500

    prime nubers task complite

--- src/Basic/PrimeNumbers.java
+++ src/Basic/PrimeNumbers.java
@@ -1,4 +1,35 @@
 package Basic;
 
+import java.util.ArrayList;
+
 public class PrimeNumbers {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0; i <= 1000; i++) {
+            arr.add(i);
+        }
+
+        //System.out.println(arr);
+
+        getPrimeNum(arr);
+    }
+    static void getPrimeNum(ArrayList<Integer> arr){
+        for (int i = 2; i <= 1000; i++) {
+            int temp = 0;
+            for (int j = 1; j < i ; j++) {
+                if (arr.get(i) % arr.get(j) == 0){
+                    temp += 1;
+                    if (temp > 1){
+                        break;
+                    }
+                }
+            }
+            if (temp == 1){
+                System.out.println(arr.get(i));
+            }
+
+        }
+    }
 }
+
